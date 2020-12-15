865f23631dbb2569cde9ffd5d1604e14aa26997a
Sam <samanthabillips@gmail.com>
2020-12-15 12:50:41 -0600

    Updated and simplified

--- PyDrill_207.py
+++ PyDrill_207.py
@@ -8,64 +8,44 @@
 # 4. Add comments throughout code
 
 
-#Parent Class
-class gameUser(): 
+#parent class New_char
+
+
+
+
+
+
+class gameUser:
     c_name = ""
     c_password = "424242"
 
-    
     def getcharinfo(self):
         c_nameEntry = input("Greetings, Wanderer! What is your name?:  ")
         c_passwordEntry = input("{}, eh? May I have the password?:  ".format(c_nameEntry))
         if (c_passwordEntry == self.c_password):
             print("Welcome, {}!\n\n Here's a gift that will prove helpful on your journey!\n\n *You recieve 5,000 Gold!*".format(c_nameEntry))
-        
-        else:
-            print("Fine. whatever. Just go on, then.\n\n*A small bag hits you in the face. You look inside the bag to find 5,000 gold!*")
 
-#child class CharInfo
+        else:
+            print("Incorrect password. You shall not pass!")
+
+#child class for gameUser
 class CharInfo(gameUser):
-    charRace1 = "Elf"
-    charRace2 = "Human"
-    charClass1 = "Bard"
-    charClass2 = "Muggle"
-    tollGold = "5000" #Changed from INT to Str
+    charRace = "Half-Elf"
+    charClass = "Bard"
+    tollGold = "5000"
 
-    def charRace(self):
-        raceEntry = input("Before you begin your journey, You must decide who your are. Are you an Elf or a human?: ")
-        if (raceEntry == self.charRace1 or raceEntry == self.charRace2):
-            print("I see. You're a {}!".format(raceEntry))
-
+#Same method; instead of c_password, use tollGold
+    def getcharinfo(self):
+        c_tollGold = input("\n\n*You approach a bridge. You need to cross it in order to get to the city, \nbut to your dismay, it's guarded by a troll. He doesn't seem happy to see you*\n\n\
+BAH! YOU NEED TO PAY THE TROLL TOLL! HOW MUCH GOLD 'AVE YA GOT?:  ")
+        if (c_tollGold == self.tollGold):
+            print("{}, is it? Well, that's enough. Carry on. ".format(c_tollGold))
         else:
-            print("Why so secretive?! Whatever. Go on. *Mumbles*")
-            
-    def charClass(self):
-        classEntry = input("Bards are natural performers, but there's nothing wrong with being a muggle! Which are you?".format(classEntry))
-        if (Class == self.charClass1 or Class == self.charClass2):
-            print("You've decided who you are. \nSome will welcome you with open arms, while others shun you.\nStand strong, wanderer. Change for no one.")
-
-        else:
-            print("My. Aren't you just plain. Whatever. Move along.")
-                
-
-    #Same method seen in Parent Class
-    #instead of c_tollGold, use tollGold
-    def Troll(self):
-        c_nameEntry = input("\n\n*You approach a bridge. You need to cross it in order to get to the city, \nbut to your dismay, it's guarded by a troll. He doesn't seem happy to see you*\n\n\"GARRGH! WHAT IS YOUR NAME?:" )
-        c_tollGold = input("BAH! NO MATTER! YOU NEED TO PAY THE TROLL TOLL! HOW MUCH GOLD 'AVE YA GOT?:  ")
-        if (c_tollGold == self.tollGold and c_nameEntry == self.c_nameEntry):
-            print("{}, is it? Well, {}, carry on. ")
-        else:
-            print("YER DON'T HAVE ENOUGH GOLD TO PAY THE TOLL! GET OUTTA HERE, YA DINGUS! /n*Ouch!*/n*The toll troll kicks you SO HARD that he/nsends you FLYING back to the starting point*")
-
-
+            print("YER DON'T HAVE ENOUGH GOLD TO PAY THE TOLL! GET OUTTA HERE, YA DINGUS!")
 
 #invokes methods inside getcharinfo and CharInfo classes
-Player1 = gameUser()
-Player1.getcharinfo()
+Player = gameUser()
+Player.getcharinfo()
 
 Character = CharInfo()
 Character.getcharinfo()
-
-
-        
