a90eedd1f225fd3885fc6b352b883cc343447e82
Sam <samanthabillips@gmail.com>
2020-12-15 13:22:16 -0600

    Fixed Inserts, but with error
    recieved sqlite3.OperationalError: table table_Files already exists  when running db in python. So, I created a new table (table_files1). Recieved error with changed file name. HOWEVER, When checking db in db sqlite, everything checks out  with the original file. It seems that every time I save under the table_files1 in python, it KEEPS inserting the same files?

--- PyDrill_185.py
+++ PyDrill_185.py
@@ -18,7 +18,7 @@ conn = sqlite3.connect('185_redo.db')
 # Table with autoincremented primary key
 with conn:
     cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS table_Files(\
+    cur.execute('CREATE TABLE IF NOT EXISTS table_Files1(\
         ID INTEGER PRIMARY KEY AUTOINCREMENT,\
         fname TEXT)')
     conn.commit()#won't work if not commited
@@ -34,14 +34,26 @@ with conn:
     fileList = ('information.docx','Hello.txt','myImage.png',\
             'myMovie.mpg','World.txt', 'data.pdf' ,'myPhoto.jpg')
     cur = conn.cursor()
-    cur.execute("fileList")
  
     for files in fileList:
-        if files.endswith("*.txt"):
-            cur.execute("INSERT INTO table_Files VALUES(?,?)")
-            cur.execute("SELECT fname FROM tbl_Files")#view corrected table with .txt files
+        if files.endswith(".txt"):
+            cur.execute("INSERT INTO table_Files1 (fname)VALUES(?)",(files,))
     conn.commit()
 conn.close()
+
+
+import sqlite3
+
+conn = sqlite3.connect('185_redo.db')
+
+cur = conn.cursor()
+with conn:
+    cur.execute("SELECT fname FROM tbl_Files1")#view corrected table with .txt files
+    results = cur.fetchall()
+    print(results)
+    conn.commit()
+conn.close()
+
 
 
 
